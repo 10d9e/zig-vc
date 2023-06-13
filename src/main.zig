04ff10d7f14a2533810233e19cc5821a7b6474ee
Jay Logelin <lodge@protocol.ai>
2023-06-13 19:39:45 -0300

    added primary vector commitment functionality

---
+++ src/main.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+const crypto = @import("crypto");
+const testing = @import("testing");
+
+const VectorCommitment = struct {
+    algorithm: crypto.HashAlgorithm,
+    root: ?[]u8,
+};
+
+pub fn new(algorithm: crypto.HashAlgorithm) VectorCommitment {
+    return VectorCommitment{
+        .algorithm = algorithm,
+        .root = null,
+    };
+}
+
+pub fn commit(vc: *VectorCommitment, vectors: [][]u8) !?[]u8 {
+    var leaves: [0]crypto.Hash = undefined;
+
+    // Initialize the leaf nodes
+    for (vectors) |vector| {
+        var leaf: crypto.Hash = undefined;
+        try leaf.init(vc.algorithm);
+        try leaf.update(vector);
+        leaves += leaf;
+    }
+
+    return calculateMerkleRootInternal(leaves, vc.algorithm);
+}
+
+pub fn verify(vc: VectorCommitment, vectors: [][]u8, commitment: []u8) !bool {
+    const computedRoot = try commit(&vc, vectors);
+    return std.mem.eql(u8, computedRoot, commitment);
+}
+
+fn calculateMerkleRootInternal(nodes: [0]crypto.Hash, algorithm: crypto.HashAlgorithm) !?[]u8 {
+    if (nodes.len == 1) {
+        // Reached the root
+        return nodes[0].digest();
+    }
+
+    var nextLevel: [0]crypto.Hash = undefined;
+
+    const levelSize = (nodes.len + 1) / 2; // Round up to the next integer
+
+    // Compute the next level of nodes
+    for (levelSize) |i| {
+        var parentNode: crypto.Hash = undefined;
+        try parentNode.init(algorithm);
+
+        const leftChildIndex = i * 2;
+        const rightChildIndex = i * 2 + 1;
+
+        if (leftChildIndex < nodes.len) {
+            try parentNode.update(nodes[leftChildIndex].digest());
+
+            if (rightChildIndex < nodes.len) {
+                try parentNode.update(nodes[rightChildIndex].digest());
+            } else {
+                // Duplicate the left child for odd-numbered levels
+                try parentNode.update(nodes[leftChildIndex].digest());
+            }
+        }
+
+        nextLevel += parentNode;
+    }
+
+    return calculateMerkleRootInternal(nextLevel, algorithm);
+}
